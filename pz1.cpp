09da8bcd637ff189f24b4e6b9a4e346c87c48696
naslitvinov <53262641+naslitvinov@users.noreply.github.com>
2024-07-12 21:20:02 +0300

    Create pz1.cpp

---
+++ pz1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+    int main() {
+        int n;
+        std::cout << "Vvedite kolichestvo chisel posledovatelnosti: ";
+        std::cin >> n;
+        double summa = 0.0;
+        for (int i = 0; i < n; i++) {
+            int num;
+            std::cout << "Vvedite " << i + 1 << "-e chislo: ";
+            std::cin >> num;
+            summa += num;
+        }
+        double average = summa / n;
+        std::cout << "Srednee arifm: " << average << std::endl;
+        return 0;
+    } 
